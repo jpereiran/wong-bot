bc71651e97892396f27680582ba46d7bc55d6762
Jose Pereira <jpereira@pucp.edu.pe>
2019-09-14 19:53:12 -0500

    Update wong.py

--- wong.py
+++ wong.py
@@ -1,61 +1,32 @@
-from flask import Flask,request,make_response
-import os,json
-import pyowm
-import os
+# import flask dependencies
+from flask import Flask, request, make_response, jsonify
 
+# initialize the flask app
 app = Flask(__name__)
-owmapikey=os.environ.get('OWMApiKey') #or provide your key here
-owm = pyowm.OWM(owmapikey)
 
-#geting and sending response to dialogflow
-@app.route('/webhook', methods=['POST'])
+
+# default route
+@app.route('/)
+def index():
+    return 'Hello World!'
+
+# function for responses
+def results():
+    # build a request object
+    req = request.get_json(force=True)
+
+    # fetch action from json
+    intent = req.get('queryResult').get('intent').get('displayName')
+
+    # return a fulfillment response
+    return {'fulfillmentText': intent}
+
+# create a route for webhook
+@app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
-    req = request.get_json(silent=True, force=True)
+    # return response
+    return make_response(jsonify(results()))
 
-    print("Request:")
-    print(json.dumps(req, indent=4))
-    
-    res = processRequest(req)
-
-    res = json.dumps(res, indent=4)
-    print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
-
-#processing the request from dialogflow
-def processRequest(req):
-    
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    observation = owm.weather_at_place(city)
-    w = observation.get_weather()
-    latlon_res = observation.get_location()
-    lat=str(latlon_res.get_lat())
-    lon=str(latlon_res.get_lon())
-     
-    wind_res=w.get_wind()
-    wind_speed=str(wind_res.get('speed'))
-    
-    humidity=str(w.get_humidity())
-    
-    celsius_result=w.get_temperature('celsius')
-    temp_min_celsius=str(celsius_result.get('temp_min'))
-    temp_max_celsius=str(celsius_result.get('temp_max'))
-    
-    fahrenheit_result=w.get_temperature('fahrenheit')
-    temp_min_fahrenheit=str(fahrenheit_result.get('temp_min'))
-    temp_max_fahrenheit=str(fahrenheit_result.get('temp_max'))
-    speech = "Today the weather in " + city + ": \n" + "Temperature in Celsius:\nMax temp :"+temp_max_celsius+".\nMin Temp :"+temp_min_celsius+".\nTemperature in Fahrenheit:\nMax temp :"+temp_max_fahrenheit+".\nMin Temp :"+temp_min_fahrenheit+".\nHumidity :"+humidity+".\nWind Speed :"+wind_speed+"\nLatitude :"+lat+".\n  Longitude :"+lon
-    
-    return {
-        "speech": speech,
-        "displayText": speech,
-        "source": "dialogflow-weather-by-satheshrgs"
-        }
-    
+# run the app
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-    print("Starting app on port %d" % port)
-    app.run(debug=False, port=port, host='0.0.0.0')
+   app.run()
